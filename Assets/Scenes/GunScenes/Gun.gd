7100c0c39c62e70e739318added6e5d7af3a74e1
BTanjerine <braydenhtanji@gmail.com>
2021-10-30 14:37:59 -0700

    trolling with gun

--- Assets/Scenes/GunScenes/Gun.gd
+++ Assets/Scenes/GunScenes/Gun.gd
@@ -2,13 +2,16 @@ extends Node2D
 
 var can_fire = true
 
-var bullet = preload("res://Scenes/Bullet.tscn")
+var bullet = preload("res://Assets/Scenes/GunScenes/Bullet.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_as_toplevel(true)
 
 func _physics_process(delta):
+	position.x = get_parent().position.x + 31
+	position.y = get_parent().position.y + 5
+	
 	var mousePos = get_global_mouse_position()
 	look_at(mousePos)
 	
