c8395e61935e576332fbafb359931fc98e79664b
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2021-10-30 09:20:04 -0700

    Trying to fix up enemy
    Enemy moves, but doesnt turn around for some reason

--- Assets/Scenes/BasicEnemy.gd
+++ Assets/Scenes/BasicEnemy.gd
@@ -1,43 +1,28 @@
-extends RigidBody2D
+extends KinematicBody2D
 
+var gravity = 10
+var velocity = Vector2()
 var is_moving_left = true
 
-var gravity = 1000
-var velocity = Vector2()
-
-var speed = 120
+export var speed = 100
 
 #func _ready():
 	#$AnimationPlayer.play("Walk")
 
-func _physics_process(delta):
-	#if $AnimationPlayer.current_animation == "Attack":
-		#return
-	
-	move_character(delta)
-	detect_turn_around()
-	
-func move_character(var delta):
+func _process(delta):
+	move_character()
+
+func move_character():
 	if is_moving_left:
-		velocity.x = -speed * delta
+		velocity.x = -speed
 	else:
-		velocity.x = speed * delta
-	linear_velocity.x = velocity.x
+		velocity.x = speed
+	
+	velocity.y += gravity
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
 
 func detect_turn_around():
-	if not $RayCast2D.is_colliding():
+	if not $RayCast2D.is_colliding() and is_on_floor():
 		is_moving_left = !is_moving_left
 		scale.x = -scale.x
-		#print("Time to turn around")
-
-func hit():
-	$AttackDetector.monitoring = true
-
-func end_of_hit():
-	$attackDetector.monitoring = false
-
-#func start_walk():
-	#AnimationPlayer.play("Walk")
-	
-#func _on_PlayerDetector_body_entered(body):
-	#animation attack
