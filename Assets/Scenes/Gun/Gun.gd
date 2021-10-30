cfacb78f31f41ee24bb61e31335076a50790f4d8
BTanjerine <braydenhtanji@gmail.com>
2021-10-30 15:39:06 -0700

    trying to fix my branch

--- Assets/Scenes/Gun/Gun.gd
+++ Assets/Scenes/Gun/Gun.gd
@@ -2,20 +2,17 @@ extends Node2D
 
 var can_fire = true
 
-var bullet = preload("res://Assets/Scenes/GunScenes/Bullet.tscn")
+export (PackedScene) var bullet
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_as_toplevel(true)
+	pass
 
 func _physics_process(delta):
-	position.x = get_parent().position.x + 31
-	position.y = get_parent().position.y + 5
-	
 	var mousePos = get_global_mouse_position()
 	look_at(mousePos)
 	
-	if(Input.is_mouse_button_pressed(1)) and can_fire:
+	if Input.is_action_pressed("shoot") and can_fire:
 
 		var bulletInstance = bullet.instance()
 		bulletInstance.position = $MuzzleArea.global_position 
--- Assets/Scenes/Player.gd
+++ Assets/Scenes/Player.gd
@@ -8,7 +8,17 @@ export var gravity = 1000
 export var fallMultiplier = 2
 export var lowJumpMultiplier = 4
 
+enum boxState {platform, carried, thrown}
+
 var _jumpReady
+
+var _facingRight
+var _grabPointSwapDistance
+
+var theBox
+
+signal facing_left
+signal facing_right
 
 var velocity = Vector2()
 
@@ -16,11 +26,21 @@ var velocity = Vector2()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_jumpReady = true
+	_facingRight = true
+	_grabPointSwapDistance = $GrabPoint.position.x
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	_lookAtMouse()
 	_movementHandler(delta)
+	_theBoxHandler()
+
+func _lookAtMouse():
+	if get_global_mouse_position().x >= position.x:
+		_lookRight()
+	else:
+		_lookLeft()
 
 func _movementHandler(var delta):
 	velocity.x = 0
@@ -28,6 +48,8 @@ func _movementHandler(var delta):
 		velocity.x -= 1
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+	
+	
 	if Input.is_action_just_pressed("jump") and _jumpReady:
 		_jumpReady = false
 		velocity.y = -jumpVel
@@ -43,7 +65,45 @@ func _movementHandler(var delta):
 	
 	velocity = move_and_slide(velocity)
 
+func _lookLeft():
+	if _facingRight:
+		_facingRight = false
+		$GrabPoint.position.x = -_grabPointSwapDistance
+		$GrabZone.scale *= -1
+		$Gun.position.x *= -1
+		$Sprite.scale.x *= -1
+		emit_signal("facing_left")
+
+func _lookRight():
+	if !_facingRight:
+		_facingRight = true
+		$GrabPoint.position.x = _grabPointSwapDistance
+		$GrabZone.scale *= -1
+		$Gun.position.x *= -1
+		$Sprite.scale.x *= -1
+		emit_signal("facing_right")
 
 func _on_GroundedChecker_body_entered(body):
 	if body.is_in_group("platforms"):
 		_jumpReady = true
+
+
+func _theBoxHandler():
+	if Input.is_action_just_pressed("grab") and theBox != null:
+		if theBox.currentState == boxState.platform:
+			theBox.call("getPickedUp", $GrabPoint, _grabPointSwapDistance)
+			connect("facing_left", theBox, "_on_Player_facing_left")
+			connect("facing_right", theBox, "_on_Player_facing_right")
+			theBox.facingRight = _facingRight
+		elif theBox.currentState == boxState.carried:
+			disconnect("facing_left", theBox, "_on_Player_facing_left")
+			disconnect("facing_right", theBox, "_on_Player_facing_right")
+			theBox.call("getThrown")
+
+func _on_GrabZone_body_entered(body):
+	if body.is_in_group("thebox"):
+		theBox = body
+
+func _on_GrabZone_body_exited(body):
+	if body.is_in_group("thebox"):
+		theBox = null
