47dbe252b3694315fbc5994afab4426857e56d63
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2021-10-31 14:54:28 -0700

    Beginnner

--- Assets/Levels/TitleMenu.gd
+++ Assets/Levels/TitleMenu.gd
@@ -7,12 +7,8 @@ func _ready():
 func _on_Start_pressed():
 	get_tree().change_scene_to(target_scene)
 
-
-
-
 func _on_About_pressed():
 	get_tree().change_scene("res://Assets/Levels/AboutPage.tscn")
-
 
 func _on_Quit_pressed():
 	get_tree().quit()
